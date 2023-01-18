5af9d8404fda6ea1db94c05b2db5c5c0e5a62b30
Sergey Reshetnikov <sreshetnikov101@gmail.com>
2023-01-18 14:30:14 +0300

    Added reverse instertion sort algorithm. Kormen's book. Task 2.1.2

--- CPP/sort.cpp
+++ CPP/sort.cpp
@@ -23,15 +23,38 @@ void insertion_sort(int arr[], int length)
 	return;
 }
 
+void r_insertion_sort(int arr[], int length)
+{
+	int key = 0;
+	int i = 0;
+
+	for (int j = length - 2; j >= 0; j--)
+	{
+		key = arr[j];
+
+		i = j + 1;
+
+		while (i <= length - 1 && arr[i] < key)
+		{
+			arr[i - 1] = arr[i];
+			i = i + 1;
+		}
+
+		arr[i - 1] = key;
+	}
+
+	return;
+}
+
 int main()
 {
-	int arr[] = { 5, 2, 4, 6, 1,3 };
+	int arr[] = { 31, 41, 59, 26, 41, 58 };
 	int arrLength = 6;
 	insertion_sort(arr, arrLength);
 	
 	for (int i = 0; i < arrLength; i++)
 	{
-		std::cout << arr[i];
+		std::cout << arr[i] << " ";
 	}
 
 	return 0;
